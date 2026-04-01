ab08abccd40f44e15907dc3ff5145f16f74cf5e1
Pratibha Jadhav <74100289+PratibhaRepos1@users.noreply.github.com>
2026-04-01 16:39:17 +0700

    created a hashtable

---
+++ DSA/hashtable.js
@@ -0,0 +1,64 @@
+let user = {
+    age: 54,
+    name: 'Kylie',
+    magic: true,
+    scream: function() {
+        console.log('ahhhhh!');
+
+    }
+}
+
+user.age // O(1)
+
+user.spell = 'hey hi'; // O(1)
+user.scream(); // O(1)
+
+// Implement Hash Table
+
+class HashTable {
+    constructor(size) {
+        this.data = new Array(size)
+    }
+
+
+_hash(key) {
+    let hash = 0;
+    for(let i=0; i< key.length; i++) {
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+    return hash;
+}
+
+set(key, value) {
+    let address = this._hash(key);
+    if(!this.data[address]) {
+        this.data[address] = [];
+        this.data[address].push([key,value]);
+
+       // console.log(this.data)
+    }
+     this.data[address].push([key,value])
+    
+}
+
+get(key) {
+    let address = this._hash(key);
+    const currentBucket = this.data[address];
+     console.log(currentBucket)
+    if(currentBucket.length) {
+        for(let i=0; i< currentBucket.length; i++) {
+            if(currentBucket[i][0] === key) {
+                return currentBucket[i][1]
+            }
+        }
+    }
+
+    return undefined;
+
+    }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 65);
+myHashTable.get('apples')
