95d3bde5477ab28519f130591cf73f7e52c74177
Pratibha Jadhav <74100289+PratibhaRepos1@users.noreply.github.com>
2026-04-03 19:55:38 +0700

    calling the counter component at app root

--- Angular21/profile-card/src/app/app.ts
+++ Angular21/profile-card/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {FormsModule } from '@angular/forms';
+import { Counter } from './components/counter/counter';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, Counter],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
