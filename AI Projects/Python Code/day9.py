a370b0616712c61a031a372be9d403719d44fcab
Pratibha Jadhav <74100289+PratibhaRepos1@users.noreply.github.com>
2026-05-27 19:39:50 +0700

    little refresh in python

---
+++ AI Projects/Python Code/day9.py
@@ -0,0 +1,19 @@
+# revision
+# Numbers and variable assignments
+a = 5
+a = 10 
+print(a+a)
+
+my_income = 100
+tax_rate = 0.1
+my_taxes = my_income * tax_rate
+print("Here is my tax: ", my_taxes)
+
+# string
+my_text = "I'm going to run"
+print(my_text)
+
+# Indexing and Slicing in String
+
+mystring = "Hello World"
+print(mystring[6])
